b2e94431b1e558aa9afdb49c544224302105063b
aedin <aedin.rundell.2021@student.lhschools.org>
2018-10-02 13:19:10 -0400

    554

--- src/rock.java
+++ src/rock.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
+import static javafx.scene.input.KeyCode.Y;
+
 public class rock {
     public static void main(String[]args){
+        Scanner key = new Scanner(System.in);
+        boolean playGame;
+        String playagin;
+        boolean y;
+        String yesOrNo;
+
+            System.out.println("This is a game of mace, knife, and sword.");
+            System.out.print(" you'll be face the computer to see who wins");
+            System.out.println("How you win is mace beats knife, knife beats sword, and sword beats mace.");
+
+
+            System.out.println("Do you want to go again?(Y/N)");
+            yesOrNo = key.nextLine();
+        do { if (){
+                System.out.println("This is a game of mace, knife, and sword.");
+                System.out.print(" you'll be face the computer to see who wins");
+                System.out.println("How you win is mace beats knife, knife beats sword, and sword beats mace.");
+            }} while (playagin.equalsIgnoreCase("y"));
+
+
 
 
 
