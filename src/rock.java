9cf61d3e57a7e0a0a81f465abcc01e7079e42ef3
aedin <aedin.rundell.2021@student.lhschools.org>
2018-10-26 14:11:47 -0400

    551

--- src/rock.java
+++ src/rock.java
@@ -1,5 +1,7 @@
+import com.sun.xml.internal.fastinfoset.util.StringArray;
 import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
 
+import java.util.Random;
 import java.util.Scanner;
 
 import static javafx.scene.input.KeyCode.Y;
@@ -8,35 +10,38 @@ public class rock {
     public static void main(String[]args){
         Scanner key = new Scanner(System.in);
         boolean playGame;
-        String playagin;
-        boolean y;
-        String yesOrNo;
-        y=true;
-       /* String computer;
+        byte yesOrNo;
+        String computer;
         String mace;
         String sword;
         String knife;
 
-        String[] arr = new String[3];
-        arr[0]= mace;
-        arr[1]= sword;
-        arr[2]=knife;*/
+
+        int[] arr = new int[]{0,1,2};
+        arr[0] = mace;
+        arr[1] = knife;
+        arr[2] = sword;
+
+       do {System.out.println("This is a game of mace, knife, and sword.");
+
+           System.out.print(" you'll be face the computer to see who wins");
+
+           System.out.println("How you win is mace beats knife, knife beats sword, and sword beats mace.");
 
 
-            System.out.println("This is a game of mace, knife, and sword.");
-            System.out.print(" you'll be face the computer to see who wins");
-            System.out.println("How you win is mace beats knife, knife beats sword, and sword beats mace.");
+           System.out.println("Do you want to go again?(1/2)");
+
+           yesOrNo = key.nextByte();
+       } while (yesOrNo == 1);
+        if (yesOrNo ==2);{
+            System.out.println("Just know your next");
+        }
 
 
-            System.out.println("Do you want to go again?(Y/N)");
-            yesOrNo = key.nextLine();
 
-       while ((yesOrNo.equalsIgnoreCase("y"))) {
-                System.out.println("This is a game of mace, knife, and sword.");
-                System.out.print(" you'll be face the computer to see who wins");
-                System.out.println("How you win is mace beats knife, knife beats sword, and sword beats mace.");
-            }if (yesOrNo.equalsIgnoreCase("n"));
-        else if (yesOrNo.equalsIgnoreCase("y"));
+
+
+
 
 
 
